555817d4e1b42ffb6c5592468d4bbe4328455551
dingnjat <annanshakai@gmail.com>
2019-02-23 21:31:22 +0700

    Initial commit

---
+++ src/main/java/me/annanjin/ckfinder/CkfinderApplication.java
@@ -0,0 +1,13 @@
+package me.annanjin.ckfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CkfinderApplication.class, args);
+    }
+
+}
